50c3400339fa010131c9be6b28f2b8e05333d0a0
adhamali450 <adhamali_4500@outlook.com>
2022-11-02 20:16:11 +0200

    InterfaceBuilder ✅

---
+++ InterfaceBuilder.py
@@ -0,0 +1,77 @@
+# import pandas as pd
+import tkinter as tk
+from tkinter import *
+from tkinter import ttk
+
+# Data
+selected_features = set()
+selected_labels = set()
+eta = 0.01
+epochs = 100
+include_bias = True
+
+
+class InterfaceBuilder:
+    common_style = {
+        'padx': 10,
+        'pady': 10,
+        'sticky': 'ew'
+    }
+
+    def __init__(self, title, data):
+        self.data = data
+
+        self.window = tk.Tk()
+        self.window.title(title)
+
+        self.curr_row = 0
+
+    def lay_cmb_selection(self, text, selectable, max, handler):
+        frame = tk.Frame(master=self.window)
+        frame.grid(
+            row=self.curr_row, padx=self.common_style['padx'], pady=self.common_style['pady'], sticky=self.common_style['sticky'])
+
+        tk.Label(frame, text=text).grid(column=0, padx=10)
+
+        for i in range(max):
+            cmb = ttk.Combobox(frame, values=selectable)
+            cmb.set("Item " + str(i + 1))
+            cmb.bind("<<ComboboxSelected>>",
+                     lambda event: handler(event.widget.get()))
+            cmb.grid(row=0, column=i + 1, padx=5)
+
+        self.curr_row += 1
+
+    def add_btn(self, text, handler):
+        btn = tk.Button(self.window, text=text,
+                        command=handler, anchor=CENTER)
+        btn.grid(
+            row=self.curr_row, padx=self.common_style['padx'], pady=self.common_style['pady'], sticky=self.common_style['sticky'])
+
+        self.curr_row += 1
+
+    def add_checkbox(self, text, handler):
+        var = IntVar()
+        cb = Checkbutton(self.window, text=text, variable=var,
+                         command=lambda: handler(var.get()))
+        cb.grid(
+            row=self.curr_row, padx=self.common_style['padx'], pady=self.common_style['pady'], sticky='w')
+
+        self.curr_row += 1
+
+    def add_entry(self, text, handler):
+        frame = tk.Frame(master=self.window)
+        frame.grid(
+            row=self.curr_row, padx=self.common_style['padx'], pady=self.common_style['pady'], sticky=self.common_style['sticky'])
+
+        tk.Label(frame, text=text).grid(column=0, padx=10)
+
+        sv = StringVar()
+        sv.trace("w", lambda name, index, mode, sv=sv: handler(sv))
+        entry = Entry(frame, textvariable=sv)
+        entry.grid(row=0, column=1, padx=5)
+
+        self.curr_row += 1
+
+    def show(self):
+        self.window.mainloop()
---
+++ main.py
@@ -0,0 +1,53 @@
+from InterfaceBuilder import InterfaceBuilder
+
+config = {
+    'selected_features': set(),
+    'selected_labels': set(),
+    'eta': 0.01,
+    'epochs': 100,
+    'include_bias': False,
+}
+
+
+def selected_feature_changed(feature):
+    config['selected_features'].add(feature)
+
+
+def selected_label_changed(label):
+    config['selected_labels'].add(label)
+
+
+def eta_changed(value):
+    try:
+        x = float(value)
+        config['eta'] = x
+    except:
+        pass
+
+
+def epochs_changed(value):
+    try:
+        x = int(value)
+        config['epochs'] = x
+    except:
+        pass
+
+
+def include_bias_changed(value):
+    config['include_bias'] = value == 1
+
+
+def sumbit_handler():
+    print(config)
+
+
+builder = InterfaceBuilder(title="Single layer preceptron", data=None)
+builder.lay_cmb_selection("Feature Selection", [
+                          1, 2, 3, 4, 5], 2, selected_feature_changed)
+builder.lay_cmb_selection(
+    "Label Selection", ['cat', 'dog', 'goat'], 2, selected_label_changed)
+builder.add_entry("Learning Rate", lambda x: eta_changed(x.get()))
+builder.add_entry("Epochs", lambda x: epochs_changed(x.get()))
+builder.add_checkbox("Include Bias", include_bias_changed)
+builder.add_btn("Train", sumbit_handler)
+builder.show()
