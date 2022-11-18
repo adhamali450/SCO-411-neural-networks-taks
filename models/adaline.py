cb2ee6c462c7781fb70c0d9d9d40ef1bb78018c2
Ahmed Tawfik <at200000000@gmail.com>
2022-11-18 19:26:39 +0200

    [Fixed] Using arrays only when train

--- models/adaline.py
+++ models/adaline.py
@@ -4,12 +4,6 @@ import numpy as np
 
 class Adaline:
     def __init__(self, X, Y, bias=False, mse_threshold=0.1) -> None:
-        try:
-            X = X.to_numpy(np.float32)
-            Y = Y.to_numpy(np.float32)
-        except:
-            pass
-
         self.X = X
         self.Y = Y
         self.y = np.zeros(len(Y))
@@ -20,10 +14,15 @@ class Adaline:
         self.mse = None
 
     def train(self, lr=0.01, epochs=1000) -> None:
+        try:
+            X = self.X.to_numpy(np.float32)
+            Y = self.Y.to_numpy(np.float32)
+        except:
+            pass
         for _ in range(epochs):
             for i in range(self.X.shape[0]):
-                x_i = self.X[i]
-                t_i = self.Y[i]
+                x_i = X[i]
+                t_i = Y[i]
                 v = np.dot(self.weights.T, x_i)
                 if self.bias:
                     v += self.b
@@ -37,12 +36,14 @@ class Adaline:
                 break
 
     def predict(self, X: pd.DataFrame) -> np.ndarray:
-
+        try:
+            X = X.to_numpy(np.float32)
+        except:
+            pass
         predictions = np.zeros(X.shape[0])
 
         for i in range(X.shape[0]):
-            x_i = X.iloc[i].values
-            v = np.dot(self.weights.T, x_i)
+            v = np.dot(self.weights.T, X[i])
             if self.bias:
                 v += self.b
             predictions[i] = np.sign(v)
